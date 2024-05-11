e7874539526dc5d251278c32a75d16d3e8f04bd6
Dima <dimaslizh@gmail.com>
2024-05-11 21:27:38 +0300

    Creating models and routes

---
+++ server/db.js
@@ -0,0 +1,20 @@
+// src/db.js
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Ошибка подключения к базе данных:', err);
+    return;
+  }
+  console.log('Подключение к базе данных установлено');
+});
+
+module.exports = connection;
