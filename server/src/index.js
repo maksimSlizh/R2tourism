accbddbe3899f214347cc2b585b05d5a6af8ba8b
Dima <dimaslizh@gmail.com>
2024-05-13 13:02:43 +0300

    Correction of static

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,7 @@ swagger(app);
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
