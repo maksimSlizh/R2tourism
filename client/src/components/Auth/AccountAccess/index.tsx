5034fa79906a634d2b4a2e3b0132bc8bb53e7abe
@Maival1 <maival.cimba@gmail.com>
2024-05-13 15:59:15 +0200

    feat(http): connect axios and create path

--- client/src/components/Auth/AccountAccess/index.tsx
+++ client/src/components/Auth/AccountAccess/index.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
 import { SINGIN_ROUTE, SINGUP_ROUTE } from '../../../utils/const'
 
@@ -18,7 +18,7 @@ export function AccountAccess() {
 
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className='account-form w-50 d-flex flex-column m-auto mt-5'>
       <div className="mb-3">
         <label className="form-label">Email address</label>
         <input
@@ -39,7 +39,7 @@ export function AccountAccess() {
       <div className='mb-3'>
         {isLogin ? <p>Don't have an account? <NavLink to={SINGUP_ROUTE}>Sing up</NavLink></p> : <p>Have an account? <NavLink to={SINGIN_ROUTE}>Sing in</NavLink></p>}
       </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
+      <button type="submit" className="btn btn-primary w-25">Submit</button>
     </form>
   )
 }
---
+++ client/src/http/user.js
@@ -0,0 +1,6 @@
+import { $host } from './api'
+
+export const registration = async(email, password) => {
+  // const {data} = await $host.post('api/user/registration', {email, password})
+}
+
