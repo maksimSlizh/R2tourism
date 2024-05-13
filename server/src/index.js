b62bd13aa66739e7a2c37c9dd82f4d99f89443b6
Dima <dimaslizh@gmail.com>
2024-05-13 12:48:38 +0300

    Maintenance of static frontend files (for the future)

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,20 @@
 // server/src/index.js
 const express = require('express');
-const swagger = require('./swagger'); 
-const placesRouter = require('./routes/places'); 
+const path = require('path');
+const swagger = require('./swagger');
+const placesRouter = require('./routes/places');
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Main');
-});
 
 app.use(placesRouter);
 
 swagger(app);
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
