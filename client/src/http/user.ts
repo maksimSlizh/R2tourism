030afd98d761db711350533a35b9f6c0c69c1c52
@Maival1 <maival.cimba@gmail.com>
2024-05-16 20:28:50 +0200

    feat(code): refactoring code

--- client/src/http/user.ts
+++ client/src/http/user.ts
@@ -1,21 +1,21 @@
-import { AxiosResponse } from 'axios';
-import { $host } from './api';
-import { AuthData } from '../types/interfaces';
+import { AxiosResponse } from 'axios'
+import { $host } from './api'
+import { AuthData } from '../types/interfaces'
 
 export const registration = async (email: string, password: string, username: string = ''): Promise<AuthData> => {
   try {
-    const response: AxiosResponse<AuthData> = await $host.post('auth/register', { email, password, username });
-    return response.data;
+    const response: AxiosResponse<AuthData> = await $host.post('auth/register', { email, password, username })
+    return response.data
   } catch (e: any) {
-    throw new Error(e.response || 'Something went wrong');
+    throw new Error(e.response || 'Something went wrong')
   }
 }
 
 export const login = async (email: string, password: string): Promise<AuthData> => {
   try {
-    const response: AxiosResponse<AuthData> = await $host.post('auth/login', { email, password });
-    return response.data;
+    const response: AxiosResponse<AuthData> = await $host.post('auth/login', { email, password })
+    return response.data
   } catch (e: any) {
-    throw new Error(e.response || 'Something went wrong');
+    throw new Error(e.response || 'Something went wrong')
   }
 }
