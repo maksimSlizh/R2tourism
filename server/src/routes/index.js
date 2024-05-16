d80fe982aa2cf2aa88d6c9aebf9ccdb7d313650e
Dima <dimaslizh@gmail.com>
2024-05-16 11:01:03 +0300

    Update swagger and routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,11 @@
-// server/src/routes/index.js
 const express = require('express');
 const router = express.Router();
 const authRouter = require('./auth');
 const placesRouter = require('./places');
+const usersRouter = require('./users');
 
 router.use('/auth', authRouter);
 router.use('/places', placesRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
